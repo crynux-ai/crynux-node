5304c7bf562a306544e6fb881db4312e08c8c291
iwehf <henry.lee@crynux.ai>
2023-08-29 21:38:17 +0800

    feature: rename celery

--- src/h_server/celery_app.py
+++ src/h_server/celery_app.py
@@ -9,6 +9,6 @@ from h_server.config import get_config
 def get_celery():
     config = get_config()
     celery = Celery(
-        "h-node", broker=config.celery.broker, backend=config.celery.backend
+        "h_worker", broker=config.celery.broker, backend=config.celery.backend
     )
     return celery
