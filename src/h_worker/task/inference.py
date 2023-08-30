5033d8aa9464d1ddf5070cad7af6a29b07045a14
iwehf <henry.lee@crynux.ai>
2023-08-30 20:15:52 +0800

    feature: download lora model from internet

--- src/h_worker/task/inference.py
+++ src/h_worker/task/inference.py
@@ -1,18 +1,13 @@
 import os
 import random
 import subprocess
-from mimetypes import guess_extension, guess_type
-from typing import Dict, Optional
-
-import httpx
+from typing import Optional
 
 from h_worker import models
 from h_worker.celery import celery
 from h_worker.config import get_config
 
-from .utils import get_image_hash
-
-http_client = httpx.Client()
+from .utils import get_lora_model, get_pose_file, upload_result
 
 
 @celery.task(name="sd_lora_inference", track_started=True)
@@ -25,112 +20,99 @@ def sd_lora_inference(
     pose: Optional[models.PoseConfig] = None,
 ):
     config = get_config()
-    base_model_path = os.path.join(
-        config.task.pretrained_models_dir, base_model, f"{base_model}.ckpt"
+    if os.path.abspath(os.getcwd()) != os.path.abspath(config.task.cwd):
+        os.chdir(config.task.cwd)
+
+    args = ["python", "sd-scripts/gen_img_diffusers.py"]
+
+    base_model_path = os.path.abspath(
+        os.path.join(
+            config.task.pretrained_models_dir, base_model, f"{base_model}.ckpt"
+        )
     )
     if not os.path.exists(base_model_path):
         raise ValueError("base model not found")
 
-    lora_model_path = os.path.join(
-        config.task.data_dir, "model", lora_model, "character.safetensors"
+    lora_model_path = get_lora_model(
+        lora_model=lora_model, data_dir=config.task.data_dir
     )
-    if not os.path.exists(lora_model_path):
-        raise ValueError("lora model not found")
 
-    image_dir = os.path.join(config.task.data_dir, "image", str(task_id))
+    image_dir = os.path.abspath(
+        os.path.join(config.task.data_dir, "image", str(task_id))
+    )
     if not os.path.exists(image_dir):
         os.makedirs(image_dir, exist_ok=True)
 
-    pose_file = ""
-    pose_dir = os.path.join(config.task.data_dir, "pose", str(task_id))
-    if not os.path.exists(pose_dir):
-        os.makedirs(pose_dir, exist_ok=True)
-
-    if pose is not None:
-        if pose["data_url"] != "":
-            data_url = pose["data_url"]
-            file_type = guess_type(url=data_url)[0]
-            file_ext = ".png"
-            if file_type is not None:
-                _file_ext = guess_extension(file_type, strict=False)
-                if _file_ext is not None:
-                    file_ext = _file_ext
-
-            pose_file = os.path.join(pose_dir, "pose" + file_ext)
-            with http_client.stream("GET", data_url) as resp:
-                resp.raise_for_status()
-                with open(pose_file, "wb") as dst:
-                    for data in resp.iter_bytes():
-                        dst.write(data)
-
-    log_file = os.path.join(config.task.inference_logs_dir, f"{task_id}.log")
-
-    envs: Dict[str, str] = {
-        "PRETRAINED_MODEL": base_model_path,
-        "MODEL_FILE": lora_model_path,
-        "OUTPUT_DIR": image_dir,
-        "PROMPTS": prompts,
-    }
-    if task_config is not None:
-        envs.update(
-            {
-                "STEPS": str(task_config["steps"]),
-                "MODEL_WEIGHT": str(task_config["lora_weight"] / 100),
-                "IMAGE_WIDTH": str(task_config["image_width"]),
-                "IMAGE_HEIGHT": str(task_config["image_height"]),
-                "NUM_IMAGES": str(task_config["num_images"]),
-            }
+    if pose is not None and len(pose["data_url"]) > 0:
+        pose_file = get_pose_file(
+            data_dir=config.task.data_dir, task_id=task_id, pose_url=pose["data_url"]
         )
+    else:
+        pose_file = ""
+
+    log_file = os.path.abspath(
+        os.path.join(config.task.inference_logs_dir, f"{task_id}.log")
+    )
+
+    args.extend(["--ckpt", base_model_path])
+    args.extend(["--outdir", image_dir])
+    args.extend(["--xformers"])
+    args.extend(["--prompt", prompts])
+    args.extend(["--sampler", "k_euler_a"])
+    args.extend(["--network_module", "networks.lora"])
+    args.extend(["--network_weights", lora_model_path])
+    args.extend(["--max_embeddings_multiples", "3"])
+    if task_config is not None:
+        args.extend(["--steps", str(task_config["steps"])])
+        args.extend(["--W", str(task_config["image_width"])])
+        args.extend(["--H", str(task_config["image_height"])])
+        args.extend(["--network_mul", str(task_config["lora_weight"] / 100)])
+        args.extend(["--images_per_prompt", str(task_config["num_images"])])
 
         seed = task_config["seed"]
         if seed == 0:
             seed = random.randint(10000000, 99999999)
-        envs["SEED"] = str(seed)
+        args.extend(["--seed", str(seed)])
 
     if pose is not None and pose_file != "":
-        openpose_model_file = os.path.join(
-            config.task.controlnet_models_dir, "control_v11p_sd15_openpose.pth"
+        openpose_model_file = os.path.abspath(
+            os.path.join(
+                config.task.controlnet_models_dir, "control_v11p_sd15_openpose.pth"
+            )
         )
 
         if os.path.exists(openpose_model_file):
-            envs.update(
-                {
-                    "POSE_IMAGE": pose_file,
-                    "POSE_PREPROCESS": "1" if pose["preprocess"] else "0",
-                    "CONTROLNET_MODEL": openpose_model_file,
-                    "POSE_WEIGHT": str(pose["pose_weight"] / 100),
-                }
-            )
+            args.extend(["--control_net_models", openpose_model_file])
+            args.extend(["--guide_image_path", pose_file])
+            args.extend(["--control_net_weights", str(pose["pose_weight"] / 100)])
+            if pose["preprocess"]:
+                args.extend(["--control_net_preps", "canny_63_191"])
+            else:
+                args.extend(["--control_net_preps", "none"])
 
-    cwd = "/app/lora-scripts"
-    if not os.path.exists(cwd):
-        raise ValueError(f"Cwd {cwd} not exists")
+    envs = os.environ.copy()
+    envs.update(
+        {
+            "HF_DATASETS_OFFLINE": "1",
+            "TRANSFORMERS_OFFLINE": "1",
+            "HF_HOME": "huggingface",
+        }
+    )
 
     with open(log_file, mode="w", encoding="utf-8") as log_file:
         subprocess.run(
-            "inference.sh",
+            args,
             stdout=log_file,
             stderr=subprocess.STDOUT,
-            cwd="/app/lora-scripts",
             env=envs,
             check=True,
             encoding="utf-8",
         )
 
-    hashes = []
-    files = []
     image_files = sorted(os.listdir(image_dir))
-    for image_file in image_files:
-        image_path = os.path.join(image_dir, image_file)
-        hashes.append(get_image_hash(image_path))
-        files.append(("files", (image_file, open(image_path, "rb"), "image/png")))
+    image_paths = [os.path.join(image_dir, file) for file in image_files]
 
-    resp = http_client.post(
-        config.task.result_url + f"/v1/task/{task_id}/result",
-        files=files,
-        data={"hashes": hashes},
-    )
-    resp.raise_for_status()
+    upload_result(config.task.result_url + f"/v1/task/{task_id}/result", image_paths)
 
 
 @celery.task(name="mock_lora_inference", track_started=True)
@@ -150,13 +132,5 @@ def mock_lora_inference(
     print(f"task config: {task_config}")
     print(f"pose config: {pose}")
 
-    hashes = [get_image_hash("test.png")]
-    files = [("files", ("test.png", open("test.png", "rb"), "image/png"))]
-
     config = get_config()
-    resp = http_client.post(
-        config.task.result_url + f"/v1/task/{task_id}/result",
-        files=files,
-        data={"hashes": hashes},
-    )
-    resp.raise_for_status()
+    upload_result(config.task.result_url + f"/v1/task/{task_id}/result", ["test.png"])
--- src/h_worker/task/utils.py
+++ src/h_worker/task/utils.py
@@ -1,5 +1,11 @@
 import os.path
 import subprocess
+import hashlib
+import httpx
+from mimetypes import guess_extension, guess_type
+from typing import List
+
+http_client = httpx.Client()
 
 
 def get_image_hash(filename: str) -> str:
@@ -7,3 +13,61 @@ def get_image_hash(filename: str) -> str:
     imhash = os.path.join(dirname, "imhash")
     res = subprocess.check_output([imhash, "-f", filename], encoding="utf-8")
     return res.strip()
+
+
+def get_lora_model(lora_model: str, data_dir: str) -> str:
+    lora_model_id = hashlib.md5(lora_model.encode("utf-8")).hexdigest()
+    lora_model_dir = os.path.abspath(
+        os.path.join(data_dir, "model", lora_model_id)
+    )
+    if not os.path.exists(lora_model_dir):
+        os.makedirs(lora_model_dir, exist_ok=True)
+
+    lora_model_path = os.path.join(lora_model_dir, "lora.safetensors")
+    if not os.path.exists(lora_model_path):
+        with http_client.stream("GET", lora_model, follow_redirects=True) as resp:
+            resp.raise_for_status()
+            with open(lora_model_path, mode="wb") as dst:
+                for data in resp.iter_bytes():
+                    dst.write(data)
+    
+    return lora_model_path
+
+
+def get_pose_file(data_dir: str, task_id: int, pose_url: str) -> str:
+    pose_dir = os.path.abspath(os.path.join(data_dir, "pose", str(task_id)))
+    if not os.path.exists(pose_dir):
+        os.makedirs(pose_dir, exist_ok=True)
+
+    file_type = guess_type(url=pose_url)[0]
+    file_ext = ".png"
+    if file_type is not None:
+        _file_ext = guess_extension(file_type, strict=False)
+        if _file_ext is not None:
+            file_ext = _file_ext
+
+    pose_file = os.path.join(pose_dir, "pose" + file_ext)
+    with http_client.stream("GET", pose_url) as resp:
+        resp.raise_for_status()
+        with open(pose_file, "wb") as dst:
+            for data in resp.iter_bytes():
+                dst.write(data)
+
+    return pose_file
+
+
+def upload_result(result_url: str, images: List[str]):
+    hashes = []
+    files = []
+    for image in images:
+        image_name = os.path.basename(image)
+        hashes.append(get_image_hash(image))
+        files.append(("files", (image_name, open(image, "rb"), "image/png")))
+
+    resp = http_client.post(
+        result_url,
+        files=files,
+        data={"hashes": hashes},
+    )
+    resp.raise_for_status()
+
