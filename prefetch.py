22b6bc16fbb6983ae728128ac590ff568a0e8d0a
iwehf <henry.lee@crynux.ai>
2023-09-01 18:55:06 +0800

    remove old prefetch.py

--- prefetch.py
+++
@@ -1,6 +0,0 @@
-from transformers import CLIPTextModel, CLIPTokenizer
-
-CLIPTokenizer.from_pretrained("openai/clip-vit-large-patch14")
-
-# Largest one
-CLIPTextModel.from_pretrained("openai/clip-vit-large-patch14")
