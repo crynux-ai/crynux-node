161386cef3f61cc9b0fdf571a4c86cdec7ea0b6a
iwehf <henry.lee@crynux.ai>
2023-09-17 22:43:51 +0800

    make get eth balance and cnx balance in get account info concurrent

--- src/h_server/server/v1/account.py
+++ src/h_server/server/v1/account.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Body, HTTPException
 from typing import Literal, Dict
 
-from anyio import get_cancelled_exc_class
+from anyio import get_cancelled_exc_class, create_task_group
 from eth_account import Account
 from typing_extensions import Annotated
 from pydantic import BaseModel, Field, SecretStr, Json
@@ -24,15 +24,28 @@ class AccountInfo(BaseModel):
 @router.get("", response_model=AccountInfo)
 async def get_account_info(*, contracts: ContractsDep):
     if contracts is not None:
-        eth_balance = await contracts.get_balance(contracts.account)
-        cnx_balance = await contracts.token_contract.balance_of(contracts.account)
-        return AccountInfo(
-            address=contracts.account, eth_balance=eth_balance, cnx_balance=cnx_balance
-        )
+        res = AccountInfo(address=contracts.account, eth_balance=0, cnx_balance=0)
+
+        async def get_eth_balance():
+            res.eth_balance = await contracts.get_balance(contracts.account)
+
+        async def get_cnx_balance():
+            res.cnx_balance = await contracts.token_contract.balance_of(
+                contracts.account
+            )
+
+        async with create_task_group() as tg:
+            tg.start_soon(get_eth_balance)
+            tg.start_soon(get_cnx_balance)
+
+        return res
     else:
         return AccountInfo(
-            address="", eth_balance=0, cnx_balance=0,
+            address="",
+            eth_balance=0,
+            cnx_balance=0,
         )
+
 
 PrivkeyType = Literal["private_key", "keystore"]
 
