30603c17ec374510ecdd579ba2c58756dd0fad25
iwehf <henry.lee@crynux.ai>
2023-10-17 17:48:01 +0800

    remove download clip model in prefetch script

--- src/prefetch.py
+++ src/prefetch.py
@@ -1,13 +1,5 @@
 from sd_task.prefetch import prefetch_models
-from huggingface_hub import snapshot_download
 
 
 if __name__ == "__main__":
     prefetch_models()
-    snapshot_download(
-        "openai/clip-vit-large-patch14",
-        cache_dir="{cache_dir}",
-        resume_download=True,
-        local_files_only=False,
-        allow_patterns=["*.json", "*.txt", "model.safetensors"]
-    )
