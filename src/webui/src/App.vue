732942368c229f269dfc69f45867d40bec71b1e9
lukew <lukew@crynux.ai>
2023-10-24 23:09:18 +0800

    [feature] add waving background image to webui

--- src/webui/src/App.vue
+++ src/webui/src/App.vue
@@ -2,6 +2,7 @@
 import { RouterView } from 'vue-router'
 import v1 from './api/v1/v1'
 import { message } from 'ant-design-vue'
+import { onBeforeUnmount, onMounted, ref } from 'vue'
 const [messageApi, contextHolder] = message.useMessage()
 
 const defaultErrorHandler = () => {
@@ -10,14 +11,39 @@ const defaultErrorHandler = () => {
 
 v1.apiServerErrorHandler = defaultErrorHandler
 v1.apiUnknownErrorHandler = defaultErrorHandler
+
+const vantaRef = ref(null)
+
+let wavesEffect = null
+onMounted(() => {
+  wavesEffect = VANTA.WAVES({
+    el: vantaRef.value,
+    waveSpeed: 0.7,
+    zoom: 1,
+    waveHeight: 10,
+    shininess: 50
+  })
+})
+onBeforeUnmount(() => {
+  if (wavesEffect) {
+    wavesEffect.destroy()
+  }
+})
 </script>
 
 <template>
-  <context-holder />
-  <RouterView />
+  <div id="bg-container" ref="vantaRef">
+    <div id="content-container">
+      <context-holder />
+      <RouterView />
+    </div>
+  </div>
 </template>
-<style lang="stylus">
-body
-    background #f5f5f5
+<style lang="stylus"></style>
+<style lang="stylus" scoped>
+#bg-container,
+    #content-container
+    position relative
+    width 100%
+    height 100%
 </style>
-<style scoped></style>
