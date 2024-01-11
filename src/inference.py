4024e344cf2bef3319567afcb3f7e53b363dc58a
iwehf <henry.lee@crynux.ai>
2024-01-11 20:49:50 +0800

    update inference: support gpt task

--- src/inference.py
+++ src/inference.py
@@ -1,47 +1,42 @@
 import argparse
+import json
 import os
 from datetime import datetime
-from typing import Optional, Type
 
 import pydantic
-from huggingface_hub.utils._errors import RepositoryNotFoundError
-from requests.exceptions import HTTPError
-
+from gpt_task.inference import run_task as gpt_run_task
+from gpt_task.models import GPTTaskArgs
 from sd_task.inference_task_args.task_args import InferenceTaskArgs
-from sd_task.inference_task_runner.errors import ModelDownloadError, TaskExecutionError
-from sd_task.inference_task_runner.inference_task import run_task
+from sd_task.inference_task_runner.inference_task import run_task as sd_run_task
 
 
-def travel_exc(e: BaseException):
-    queue = [e]
-    exc_set = set(queue)
-
-    while len(queue) > 0:
-        exc = queue.pop(0)
-        yield exc
-        if exc.__cause__ is not None and exc.__cause__ not in exc_set:
-            queue.append(exc.__cause__)
-            exc_set.add(exc.__cause__)
-        if exc.__context__ is not None and exc.__context__ not in exc_set:
-            queue.append(exc.__context__)
-            exc_set.add(exc.__context__)
+def sd_inference(output_dir: str, task_args_str: str):
+    print(f"Inference task starts at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+    task_args = InferenceTaskArgs.model_validate_json(task_args_str)
+    imgs = sd_run_task(task_args)
+    for i, img in enumerate(imgs):
+        dst = os.path.join(output_dir, f"{i}.png")
+        img.save(dst)
+    print(f"Inference task finishes at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
 
 
-def match_exception(
-    e: Exception, target: Type[Exception], message: Optional[str] = None
-) -> bool:
-    for exc in travel_exc(e):
-        if isinstance(exc, target) and (message is None or message in str(exc)):
-            return True
-    return False
+def gpt_inference(output_dir: str, task_args_str: str):
+    task_args = GPTTaskArgs.model_validate_json(task_args_str)
+    resp = gpt_run_task(task_args)
+
+    dst = os.path.join(output_dir, "response.json")
+    with open(dst, mode="w", encoding="utf-8") as f:
+        json.dump(resp, f, ensure_ascii=False)
 
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument("task_type", type=int, choices=[0, 1])
     parser.add_argument("output_dir", type=str)
     parser.add_argument("task_args", type=str)
     args = parser.parse_args()
 
+    task_type: int = args.task_type
     output_dir: str = args.output_dir
     task_args_str: str = args.task_args
 
@@ -49,25 +44,21 @@ def main():
         print(
             f"Inference task starts at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
         )
-        task_args = InferenceTaskArgs.model_validate_json(task_args_str)
-        imgs = run_task(task_args)
-        for i, img in enumerate(imgs):
-            dst = os.path.join(output_dir, f"{i}.png")
-            img.save(dst)
+
+        if task_type == 0:
+            # sd task
+            sd_inference(output_dir, task_args_str)
+        elif task_type == 0:
+            # gpt task
+            gpt_inference(output_dir, task_args_str)
+        else:
+            raise ValueError(f"unknown task type {task_type}")
+
         print(
             f"Inference task finishes at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
         )
     except pydantic.ValidationError:
-        print("Task args validation error")
-        raise
-    except TaskExecutionError:
-        print("Task execution error")
-        raise
-    except ModelDownloadError as e:
-        if match_exception(e, RepositoryNotFoundError) or match_exception(e, HTTPError):
-            print("Task model not found")
-        else:
-            print("Task download error")
+        print("Task args invalid")
         raise
 
 
