9f93ed3ecc399a6d82d545db54347d236ed900bd
aaronyuai <c@crynux.ai>
2024-03-05 00:03:27 -0800

    Support MacOS package and node start

--- build/macos/build.sh
+++ build/macos/build.sh
@@ -1,3 +1,10 @@
+# Build package from source
+# Example call: bash build/macos/build.sh ~/crynux_app ~/crynux.tar.gz
+WORK_DIR=$1
+OUTPUT_FILE=$2
+echo $WORK_DIR
+echo $OUTPUT_FILE
+
 
 if ! [ -x "$(command -v brew)" ]; then
   echo 'Error: brew is not installed.'
@@ -8,32 +15,31 @@ function check_or_install {
   if ! [ -x "$(command -v $1)" ]
   then
     echo "Error: $1 is not installed."
-    brew install "$1"
+    brew install "$2"
   fi
 }
 
-check_or_install git
-check_or_install yarn
-check_or_install node
-check_or_install python3
+check_or_install git git
+check_or_install yarn yarn
+check_or_install node node
+check_or_install go go
+check_or_install python3.10 python@3.10
 
 GIT_DIR=$(pwd)
-git submodule update --init --recursive
-rm -R -f ~/crynux_app
-mkdir ~/crynux_app
-cd ~/crynux_app
+mkdir $WORK_DIR
+cd $WORK_DIR
 
-cp -R "$GIT_DIR/src/webui" ~/crynux_app/
-cd ~/crynux_app/webui
+cp -R "$GIT_DIR/src/webui" $WORK_DIR
+cd "$WORK_DIR/webui"
 cp src/config.example.json  src/config.json
 yarn && yarn build
-cd ~/crynux_app
+cd $WORK_DIR
 
 ### Server ###
-python3 -m venv server-venv
-export PATH="~/crynux_app/venv/bin:${PATH}"
-mkdir ~/crynux_app/crynux-node
-cd ~/crynux_app/crynux-node
+python3.10 -m venv venv
+export PATH="$WORK_DIR/venv/bin:${PATH}"
+mkdir "$WORK_DIR/crynux-node"
+cd "$WORK_DIR/crynux-node"
 cp -R $GIT_DIR/src src
 cp $GIT_DIR/pyproject.toml pyproject.toml
 cp $GIT_DIR/setup.py setup.py
@@ -45,32 +51,30 @@ pip install -r requirements.txt && pip install .
 
 
 ### worker ###
-python3 -m venv worker-venv
+cd $GIT_DIR
+git submodule update --init --recursive
+cd $WORK_DIR
+mkdir "$WORK_DIR/worker"
+cp $GIT_DIR/src/prefetch.py worker/prefetch.py
+cp $GIT_DIR/src/inference.py worker/inference.py
+
+
+python3.10 -m venv worker/venv
+source "$WORK_DIR/worker/venv/bin/activate"
+
 cp -R $GIT_DIR/stable-diffusion-task stable-diffusion-task
 cd stable-diffusion-task
 pip install -r requirements_macos.txt
 pip install .
-cd ~/crynux_app
+cd $WORK_DIR
 
 cp -R $GIT_DIR/gpt-task gpt-task
 cd gpt-task
 pip install -r requirements_macos.txt
 pip install .
-cd ~/crynux_app
+cd $WORK_DIR
 
-mkdir ~/crynux_app/worker
-cp $GIT_DIR/src/prefetch.py worker/prefetch.py
-cp $GIT_DIR/src/inference.py worker/inference.py
-
-mkdir data
-mkdir data/external
-mkdir data/huggingface
-mkdir data/results
-mkdir data/inference-logs
 mkdir config
-
-cp $GIT_DIR/config/config.yml.example config/config.yml
+cp $GIT_DIR/config/config.yml.macos_example config/config.yml
 cp $GIT_DIR/start.sh start.sh
-export CRYNUX_SERVER_CONFIG="$(pwd)/config/config.yml"
-python3 -m crynux_server.main run
-
+tar czf $OUTPUT_FILE .
