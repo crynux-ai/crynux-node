87eb8ad54ab5257aad8ebde097434da32ed0d5b5
iwehf <henry.lee@crynux.ai>
2024-03-23 16:08:47 +0800

    update node and task contract

--- src/crynux_server/models/event.py
+++ src/crynux_server/models/event.py
@@ -2,23 +2,36 @@ from typing import List, Literal
 
 from eth_typing import ChecksumAddress
 from pydantic import BaseModel, Field
-from web3.types import EventData
 from web3 import Web3
+from web3.types import EventData
 
 from .task import TaskType
 
 TaskKind = Literal[
+    "TaskPending",
     "TaskStarted",
     "TaskResultReady",
     "TaskResultCommitmentsReady",
     "TaskSuccess",
     "TaskAborted",
+    "TaskResultUploaded",
+    "TaskNodeSuccess",
+    "TaskNodeSlashed",
+    "TaskNodeCancelled",
 ]
 
 
 class TaskEvent(BaseModel):
     kind: TaskKind = Field(init_var=False)
     task_id: int
+
+
+class TaskPending(TaskEvent):
+    kind: TaskKind = Field(default="TaskPending", init_var=False, frozen=True)
+    task_type: TaskType
+    creator: ChecksumAddress
+    task_hash: str
+    data_hash: str
 
 
 class TaskStarted(TaskEvent):
@@ -51,10 +64,33 @@ class TaskSuccess(TaskEvent):
 
 class TaskAborted(TaskEvent):
     kind: TaskKind = Field(default="TaskAborted", init_var=False, frozen=True)
+    reason: str
+
+
+class TaskResultUploaded(TaskEvent):
+    kind: TaskKind = Field(default="TaskResultUploaded", init_var=False, frozen=True)
+
+
+class TaskNodeSuccess(TaskEvent):
+    kind: TaskKind = Field(default="TaskNodeSuccess", init_var=False, frozen=True)
+    node_address: ChecksumAddress
+    fee: int
+
+
+class TaskNodeSlashed(TaskEvent):
+    kind: TaskKind = Field(default="TaskNodeSlashed", init_var=False, frozen=True)
+    node_address: ChecksumAddress
+
+
+class TaskNodeCancelled(TaskEvent):
+    kind: TaskKind = Field(default="TaskNodeCancelled", init_var=False, frozen=True)
+    node_address: ChecksumAddress
 
 
 def load_event_from_json(kind: TaskKind, event_json: str) -> TaskEvent:
-    if kind == "TaskStarted":
+    if kind == "TaskPending":
+        return TaskPending.model_validate_json(event_json)
+    elif kind == "TaskStarted":
         return TaskStarted.model_validate_json(event_json)
     elif kind == "TaskResultReady":
         return TaskResultReady.model_validate_json(event_json)
@@ -64,13 +100,29 @@ def load_event_from_json(kind: TaskKind, event_json: str) -> TaskEvent:
         return TaskSuccess.model_validate_json(event_json)
     elif kind == "TaskAborted":
         return TaskAborted.model_validate_json(event_json)
+    elif kind == "TaskResultUploaded":
+        return TaskResultUploaded.model_validate_json(event_json)
+    elif kind == "TaskNodeSuccess":
+        return TaskNodeSuccess.model_validate_json(event_json)
+    elif kind == "TaskNodeCancelled":
+        return TaskNodeCancelled.model_validate_json(event_json)
+    elif kind == "TaskNodeSlashed":
+        return TaskNodeSlashed.model_validate_json(event_json)
     else:
         raise ValueError(f"unknown event kind {kind} from json")
 
 
 def load_event_from_contracts(event_data: EventData) -> TaskEvent:
     name = event_data["event"]
-    if name == "TaskStarted":
+    if name == "TaskPending":
+        return TaskPending(
+            task_id=event_data["args"]["taskId"],
+            task_type=event_data["args"]["taskType"],
+            creator=Web3.to_checksum_address(event_data["args"]["creator"]),
+            task_hash=Web3.to_hex(event_data["args"]["taskHash"]),
+            data_hash=Web3.to_hex(event_data["args"]["dataHash"]),
+        )
+    elif name == "TaskStarted":
         return TaskStarted(
             task_id=event_data["args"]["taskId"],
             task_type=event_data["args"]["taskType"],
@@ -89,6 +141,26 @@ def load_event_from_contracts(event_data: EventData) -> TaskEvent:
             result_node=Web3.to_checksum_address(event_data["args"]["resultNode"]),
         )
     elif name == "TaskAborted":
-        return TaskAborted(task_id=event_data["args"]["taskId"])
+        return TaskAborted(
+            task_id=event_data["args"]["taskId"], reason=event_data["args"]["reason"]
+        )
+    elif name == "TaskResultUploaded":
+        return TaskResultUploaded(task_id=event_data["args"]["taskId"])
+    elif name == "TaskNodeSuccess":
+        return TaskNodeSuccess(
+            task_id=event_data["args"]["taskId"],
+            node_address=Web3.to_checksum_address(event_data["args"]["nodeAddress"]),
+            fee=event_data["args"]["fee"],
+        )
+    elif name == "TaskNodeSlashed":
+        return TaskNodeSlashed(
+            task_id=event_data["args"]["taskId"],
+            node_address=Web3.to_checksum_address(event_data["args"]["nodeAddress"]),
+        )
+    elif name == "TaskNodeCancelled":
+        return TaskNodeCancelled(
+            task_id=event_data["args"]["taskId"],
+            node_address=Web3.to_checksum_address(event_data["args"]["nodeAddress"]),
+        )
     else:
         raise ValueError(f"unknown event kind {name} from contracts")
