c2669044060f21dcbba7fb65c60255c03b4471cb
aaronyuai <c@crynux.ai>
2024-04-09 08:56:51 -0700

    fix script

--- build/macos/install.sh
+++ build/macos/install.sh
@@ -4,5 +4,5 @@ MACAPP_CHECKSUM=""
 if [[ -n $(shasum crynux.dmg | grep $MACAPP_CHECKSUM) ]]; then
     # Unsigned app need to be explicitly handled with.
     sudo xattr -rds com.apple.quarantine crynux.dmg
-    open crynux.dmg
+    open Crynux.dmg
 fi
