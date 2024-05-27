350228955888222ce78955faad556b1dc7b45caf
iwehf <henry.lee@crynux.ai>
2024-05-27 12:07:45 +0800

    change task state api: return correct task num when node state is stopped

--- src/crynux_server/server/v1/task.py
+++ src/crynux_server/server/v1/task.py
@@ -86,34 +86,43 @@ async def get_task_stats(
     *, task_state_cache: TaskStateCacheDep, state_cache: ManagerStateCacheDep
 ):
     node_status = (await state_cache.get_node_state()).status
-    if task_state_cache is None or node_status not in [
+    num_today = 0
+    num_total = 0
+    if node_status not in [
         NodeStatus.Running,
         NodeStatus.PendingPause,
         NodeStatus.PendingStop,
     ]:
-        return TaskStats(status="stopped", num_today=0, num_total=0)
-    running_status = [
-        TaskStatus.Pending,
-        TaskStatus.Executing,
-        TaskStatus.ResultUploaded,
-        TaskStatus.Disclosed,
-        TaskStatus.ResultFileUploaded,
-    ]
-    running_states = await task_state_cache.find(status=running_status)
-    if len(running_states) > 0:
-        status = "running"
+        status = "stopped"
+    elif task_state_cache is None:
+        status =  "idle"
     else:
-        status = "idle"
+        running_status = [
+            TaskStatus.Pending,
+            TaskStatus.Executing,
+            TaskStatus.ResultUploaded,
+            TaskStatus.Disclosed,
+            TaskStatus.ResultFileUploaded,
+        ]
+        running_states = await task_state_cache.find(status=running_status)
+        if len(running_states) > 0:
+            status = "running"
+        else:
+            status = "idle"
+    
+    if task_state_cache is not None:
+        now = datetime.now().astimezone()
+        date = now.date()
+        today_ts = time.mktime(date.timetuple())
+        today = datetime.fromtimestamp(today_ts)
 
-    now = datetime.now().astimezone()
-    date = now.date()
-    today_ts = time.mktime(date.timetuple())
-    today = datetime.fromtimestamp(today_ts)
+        today_states = await task_state_cache.find(start=today, status=[TaskStatus.Success])
 
-    today_states = await task_state_cache.find(start=today, status=[TaskStatus.Success])
+        total_states = await task_state_cache.find(status=[TaskStatus.Success])
 
-    total_states = await task_state_cache.find(status=[TaskStatus.Success])
+        num_today = len(today_states)
+        num_total = len(total_states)
 
     return TaskStats(
-        status=status, num_today=len(today_states), num_total=len(total_states)
+        status=status, num_today=num_today, num_total=num_total
     )
