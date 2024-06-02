9c1037235992e4872959c24a393ce71e586c0497
lukew <lukew@crynux.ai>
2024-06-02 22:40:04 +0800

    [bug-fix] syntax error

--- build/linux-server/tools.sh
+++ build/linux-server/tools.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 
 # Golang
-if !command -v go version &> /dev/null
+if ! command -v go version &> /dev/null
 then
+    echo "Installing Golang..."
     wget https://go.dev/dl/go1.21.10.linux-amd64.tar.gz
     tar -C /usr/local -xzf go1.21.10.linux-amd64.tar.gz
     mkdir /usr/local/golib
@@ -12,8 +13,9 @@ then
 fi
 
 # Nodejs
-if !command -v node --version &> /dev/null
+if ! command -v node --version &> /dev/null
 then
+    echo "Installing Nodejs..."
     # installs fnm (Fast Node Manager)
     curl -fsSL https://fnm.vercel.app/install | bash
     fnm use --install-if-missing 22
@@ -22,5 +24,6 @@ fi
 # Yarn
 if ! command -v yarn --version &> /dev/null
 then
+    echo "Installing Yarn..."
     npm install -g yarn
 fi
