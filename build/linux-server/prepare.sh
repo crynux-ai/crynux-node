62c958e11cb4cd3422f95f0953b4fb863e81b36c
lukew <lukew@crynux.ai>
2024-06-03 07:58:16 +0800

    [feature] use prepared source as the source package

--- build/linux-server/prepare.sh
+++ build/linux-server/prepare.sh
@@ -95,7 +95,7 @@ cd $WORK_DIR
 mkdir config
 cp $GIT_DIR/config/config.yml.package_example config/config.yml
 cp $GIT_DIR/start.sh start.sh
-cp $GIT_DIR/build/linux-server/* .
+cp -r $GIT_DIR/build/linux-server/* .
 chmod +x *.sh
 
 
