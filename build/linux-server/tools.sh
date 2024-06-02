82178f5f176155dd6b95e98e9b8bcbdfa0ce2716
lukew <lukew@crynux.ai>
2024-06-03 07:44:34 +0800

    [bug-fix] update tools

--- build/linux-server/tools.sh
+++ build/linux-server/tools.sh
@@ -1,24 +1,43 @@
 #! /bin/bash
 
+INSTALL=$1
+
+if [[ -z $INSTALL ]]; then
+  echo "Please specify the installation path"
+  exit 1
+fi
+
 # Golang
+GOPATH="$INSTALL/golib"
+export GOPATH
+export PATH=$PATH:$INSTALL/go/bin:$GOPATH/bin
+
+if [ ! -d "$GOPATH" ]; then
+    mkdir "$GOPATH"
+fi
+
 if ! command -v go version &> /dev/null
 then
     echo "Installing Golang..."
     wget https://go.dev/dl/go1.21.10.linux-amd64.tar.gz
-    tar -C ~/go -xzf go1.21.10.linux-amd64.tar.gz
-    mkdir ~/golib
-    GOPATH=~/golib
-    export GOPATH
-    export PATH=$PATH:~/go/bin:$GOPATH/bin
+    tar -C "$INSTALL" -xzf go1.21.10.linux-amd64.tar.gz
 fi
 
 # Nodejs
+chmod +x ~/.bashrc
+~/.bashrc
+
+if ! command -v fnm --version &> /dev/null
+then
+    echo "Installing fnm..."
+    # installs fnm (Fast Node Manager)
+    curl -fsSL https://fnm.vercel.app/install | bash
+    ~/.bashrc
+fi
+
 if ! command -v node --version &> /dev/null
 then
     echo "Installing Nodejs..."
-    # installs fnm (Fast Node Manager)
-    curl -fsSL https://fnm.vercel.app/install | bash
-    source ~/.bashrc
     fnm use --install-if-missing 22
 fi
 
